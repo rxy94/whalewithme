d9a48009c11e47fdd9bc62bf48a4ff57071f39e4
Ruyi Xia Ye <ruyi1994@gmail.com>
2023-12-07 01:06:59 +0100

    añado una galería tipo slideshow

--- js/whale.js
+++ js/whale.js
@@ -15,6 +15,35 @@ function myFunction() {
     }
 }
 
+
+/******************************************** Slideshow Gallery *******************************************/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
 /********************************    Código del modal del footer  *****************************************/
 var modal = document.getElementById("myModal");
 
